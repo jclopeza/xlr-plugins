cb3ac28b7032747d21853d00fcbc4918e91c147e
José Carlos <josecarlos.lopezayala@gmail.com>
2019-11-26 18:44:46 +0100

    new value provider

--- xlr-jira-projects-provider-plugin/jiraComponentValueProvider/GetComponents.py
+++ xlr-jira-projects-provider-plugin/jiraComponentValueProvider/GetComponents.py
@@ -1,8 +1,8 @@
 import json
 
 request = HttpRequest({'url': valueProvider.jiraServer.url}, valueProvider.jiraServer.username, valueProvider.jiraServer.password)
-response = request.get("/rest/api/2/project", contentType = 'application/json')
+response = request.get("/rest/api/2/project/{0}/components".format(valueProvider.project), contentType = 'application/json')
 data = json.loads(response.getResponse())
 result = []
 for repository in data:
-    result = result + [repository['key']]
+    result = result + [repository['name']]
